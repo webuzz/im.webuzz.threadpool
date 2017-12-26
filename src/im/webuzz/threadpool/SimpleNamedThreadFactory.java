996fcbb19c10516e1b06223a7203a9a258f0d8d8
Zhou Renjian <zhourenjian@gmail.com>
2017-12-26 17:03:36 +0800

    Change spaces to tabs

--- src/im/webuzz/threadpool/SimpleNamedThreadFactory.java
+++ src/im/webuzz/threadpool/SimpleNamedThreadFactory.java
@@ -17,28 +17,28 @@ import java.util.concurrent.ThreadFactory;
  *
  */
 public class SimpleNamedThreadFactory implements ThreadFactory {
-    final ThreadGroup group;
-    final AtomicInteger threadNumber = new AtomicInteger(1);
-    protected String namePrefix;
+	final ThreadGroup group;
+	final AtomicInteger threadNumber = new AtomicInteger(1);
+	protected String namePrefix;
 
-    public SimpleNamedThreadFactory(String prefix) {
-        SecurityManager s = System.getSecurityManager();
-        group = (s != null)? s.getThreadGroup() : Thread.currentThread().getThreadGroup();
-        updatePrefix(prefix);
-    }
+	public SimpleNamedThreadFactory(String prefix) {
+		SecurityManager s = System.getSecurityManager();
+		group = (s != null)? s.getThreadGroup() : Thread.currentThread().getThreadGroup();
+		updatePrefix(prefix);
+	}
 
-    public Thread newThread(Runnable r) {
-        Thread t = new Thread(group, r, namePrefix + threadNumber.getAndIncrement(), 0);
-        if (t.getPriority() != Thread.NORM_PRIORITY)
-            t.setPriority(Thread.NORM_PRIORITY);
-        return t;
-    }
+	public Thread newThread(Runnable r) {
+		Thread t = new Thread(group, r, namePrefix + threadNumber.getAndIncrement(), 0);
+		if (t.getPriority() != Thread.NORM_PRIORITY)
+			t.setPriority(Thread.NORM_PRIORITY);
+		return t;
+	}
 
-    public void updatePrefix(String prefix) {
-    	if (prefix == null || prefix.length() == 0) {
-    		namePrefix = "Thread-";
-    	} else {
-    		namePrefix = prefix + "-";
-    	}
-    }
+	public void updatePrefix(String prefix) {
+		if (prefix == null || prefix.length() == 0) {
+			namePrefix = "Thread-";
+		} else {
+			namePrefix = prefix + "-";
+		}
+	}
 }
